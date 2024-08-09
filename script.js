9f34f3fd683fb0282b0c7c698060b60dd95ef1a8
Tiago M Borges <tmborges.dev@gmail.com>
2024-08-09 20:49:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario() {
+    formulario.style.left = "20%"
+}
+
+function hideform() {
+    formulario.style.left = "-70%"
+}
